f3ba704aa0b69d04acd5733bb45bd6ac59141c0b
XATTpy <zoomerrr@mail.ru>
2019-10-28 22:26:14 +0300

    new models and migrates (admin, user, task, comment)

---
+++ db/migrate/20191028150854_create_tasks.rb
@@ -0,0 +1,15 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.belongs_to :user
+      t.string :subject
+      t.string :assignee
+      t.string :status
+      t.text :description
+      t.string :created_by
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
